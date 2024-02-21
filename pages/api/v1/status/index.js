bf9c7b37b663a6fd078214e558e020c03852f656
haasdev33 <58893001+haasdev33@users.noreply.github.com>
2024-02-21 00:41:30 +0000

    adicionado `.env` e conexção com postgres

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,8 @@
-import database from "../../../../infra/database";
-function status(request, response) {
-  response.status(200).json({ chave: "valor" });
+import database from "../../../../infra/database.js";
+async function status(request, response) {
+  const result = await database.query("SELECT 66 + 56.5;");
+  console.log(result);
+  response.status(200).json({ chave: result });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,3 @@
-
 test('GET na Api 200', async () => {
   const response = await fetch("http://localhost:3000/api/v1/status")
     expect(response.status).toBe(200)
